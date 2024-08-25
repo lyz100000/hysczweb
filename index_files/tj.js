50373c70f4c9ccda4ee4caabc66be235eb42e5f3
lyz100000 <90488404+lyz100000@users.noreply.github.com>
2024-08-25 19:40:50 +0800

    Add files via upload

---
+++ index_files/tj.js
@@ -0,0 +1,43 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "http://web.archive.org/web/20240117050309/https://hm.baidu.com/hm.js?92658b22bc643eda74d393a8cac0734c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+}
+/*
+     FILE ARCHIVED ON 05:03:09 Jan 17, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 10:38:07 Aug 25, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.038
+  exclusion.robots: 0.043
+  exclusion.robots.policy: 0.017
+  esindex: 0.025
+  cdx.remote: 8.304
+  LoadShardBlock: 207.222 (3)
+  PetaboxLoader3.datanode: 722.845 (5)
+  PetaboxLoader3.resolve: 221.906 (2)
+  load_resource: 841.252
+  loaddict: 140.113
+*/
